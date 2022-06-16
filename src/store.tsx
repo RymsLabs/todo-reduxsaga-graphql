2c75abcb52fe23d8e83223ee894d840d92e89bf4
ManujBahety <e19cse230@bennett.edu.in>
2022-06-16 12:28:16 +0530

    Adds local storage

--- src/store.tsx
+++ src/store.tsx
@@ -1,7 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import rootReducer from "./reducers/index";
 
-const store = configureStore({ reducer: rootReducer });
-
+const persistConfig = {
+  key: "persist=root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({ reducer: persistedReducer });
+export const persistor = persistStore(store);
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
